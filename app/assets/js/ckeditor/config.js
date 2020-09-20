784e2b46f3489a5460fa17bfcfae2af87a73115b
Josh Rogers <mail@joshlrogers.com>
2020-09-19 20:36:39 -0700

    use the cdn, but reconfigure app php library

--- app/assets/js/ckeditor/config.js
+++ app/assets/js/ckeditor/config.js
@@ -4,9 +4,6 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-    config.plugins = 'dialogui,dialog,about,a11yhelp,dialogadvtab,basicstyles,bidi,blockquote,notification,button,toolbar,clipboard,panelbutton,panel,floatpanel,colorbutton,colordialog,templates,menu,contextmenu,copyformatting,div,resize,elementspath,enterkey,entities,popup,filetools,filebrowser,find,fakeobjects,flash,floatingspace,listblock,richcombo,font,forms,format,horizontalrule,htmlwriter,iframe,wysiwygarea,image,indent,indentblock,indentlist,smiley,justify,menubutton,language,link,list,liststyle,magicline,maximize,newpage,pagebreak,pastetext,pastetools,pastefromgdocs,pastefromword,preview,print,removeformat,save,selectall,showblocks,showborders,sourcearea,specialchar,scayt,stylescombo,tab,table,tabletools,tableselection,undo,lineutils,widgetselection,widget,notificationaggregator,uploadwidget,uploadimage,wsc,showprotected,sourcedialog,codesnippet,stylesheetparser';
-    config.skin = 'moono-lisa';
-
     /* Encoding */
     config.entities                     = false;
     config.entities_latin               = false;

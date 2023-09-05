8eb207c58484af4069185c63f9a092d9142ba71d
josh <mail@joshlrogers.com>
2023-09-04 18:42:50 -0700

    readme update, cleanup server environment initiation

--- app/setup/constants.php
+++ app/setup/constants.php
@@ -12,7 +12,7 @@
 
 define('PAGE_LOAD_START', microtime(true));
 
-define('APP_VERSION', '0.6.0');
+define('APP_VERSION', '0.6.1');
 
 define('WEB_ROOT', dirname(__DIR__));
 define('DB_ROOT', realpath(WEB_ROOT . '/../db'));
@@ -23,13 +23,10 @@ define('GASEOUS_AUTOLOADER', WEB_ROOT . '/libraries/Autoload.php');
 define('ENVIRONMENT_INI', WEB_ROOT . '/setup/environments.ini');
 define('DEFAULT_ENVIRONMENT', 'default');
 
-if (PHP_SAPI != 'cli') {
-    define('ENVIRONMENT', $_SERVER['ENVIRONMENT'] ?? DEFAULT_ENVIRONMENT);
-} else {
-    $environment_param = filter_var($argv[1] ?? null, FILTER_SANITIZE_STRING, FILTER_NULL_ON_FAILURE);
+$server_environment = PHP_SAPI == 'cli'
+    ? (!empty($argv[1]) ? $argv[1] : DEFAULT_ENVIRONMENT)
+    : (!empty($_SERVER['ENVIRONMENT']) ? $_SERVER['ENVIRONMENT'] : DEFAULT_ENVIRONMENT);
 
-    define('ENVIRONMENT', $environment_param ?? DEFAULT_ENVIRONMENT);
-}
-
+define('ENVIRONMENT', $server_environment ?? DEFAULT_ENVIRONMENT);
 define('LOGIN_COOKIE', ($_SERVER['LOGIN_COOKIE'] ?? 'login'));
 define('SESSION_NAME', ($_SERVER['SESSION_NAME'] ?? 'session'));
